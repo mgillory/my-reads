45145752a07551085a7b79f998ac85f425e05b6b
mgillory <mathfreitas15@gmail.com>
2018-10-06 22:53:36 -0300

    feat, refactor, style: grabed all functionalities and style already implemented on my fork to later work on new features and styles

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,80 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+import '../App.css';
 
 export default class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoverOn: -1
+    }
+  }
+
+  findIndex = (id) => {
+    const { books } = this.props;
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].id === id) {
+        return i;
+      }
+    }
+  }
+
+  onChange = (e, book) => {
+    const { books, handleChange, booksOnTheShelf } = this.props;
+    const bookIndex = this.findIndex(book.id);
+    books[bookIndex].shelf = e.target.value;
+    handleChange(books[bookIndex], e.target.value, booksOnTheShelf ? true : false);
+  }
+
+  isBookOnTheShelf = (bookId) => {
+    const { booksOnTheShelf } = this.props;
+    const book = booksOnTheShelf && booksOnTheShelf.find(book => book.id === bookId);
+    return book ? book.shelf : false;
+  }
+
+  onClick = (bookIndex) => {
+    console.log(bookIndex);
+  }
+
+  onMouseEnter = (bookIndex) => {
+    this.setState({ hoverOn: bookIndex });
+  }
+
+  onMouseLeave = () => {
+    this.setState({ hoverOn: -1 });
+  }
+
   render() {
+    const { books } = this.props;
+    const { hoverOn } = this.state;
     return (
-      <div>
-        BookList
-      </div>
+      <ol className="books-grid">
+        {books && Array.isArray(books) && books.map((book, i) => (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className={classnames('book-cover', { 'has-positive-translate': hoverOn > -1 && hoverOn < i ? true : false }, { 'has-negative-translate': hoverOn > -1 && hoverOn > i ? true : false })}
+                  onClick={() => this.onClick(i)}
+                  onMouseEnter={() => this.onMouseEnter(i)}
+                  onMouseLeave={this.onMouseLeave}
+                  style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail})` }} />
+                <div className={classnames('book-shelf-changer', { 'has-positive-translate': hoverOn > -1 && hoverOn < i ? true : false }, { 'has-negative-translate': hoverOn > -1 && hoverOn > i ? true : false })} style={hoverOn === i ? { display: 'none' } : {}}>
+                  <select value={this.isBookOnTheShelf(book.id) || book.shelf || 'none'} onChange={e => this.onChange(e, book)}>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className={classnames('book-title', { 'has-positive-translate': hoverOn > -1 && hoverOn < i ? true : false }, { 'has-negative-translate': hoverOn > -1 && hoverOn > i ? true : false })}>{book.title}</div>
+              <div className={classnames('book-authors', { 'has-positive-translate': hoverOn > -1 && hoverOn < i ? true : false }, { 'has-negative-translate': hoverOn > -1 && hoverOn > i ? true : false })}>{book.authors}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
     );
   }
 }
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PulseLoader } from 'react-spinners';
+import Search from './Search';
+import BookList from './BookList';
+import '../App.css';
 
 export default class BookShelf extends Component {
   render() {
+    const {
+      books, sections, handleChange, loading,
+    } = this.props;
+    console.log(books);
+
+    const renderSections = sections.map(section => (
+      <div key={section.name} className="bookshelf">
+        <h2 className="bookshelf-title">{section.title}</h2>
+        <div className="bookshelf-books">
+          <PulseLoader
+            className="loader"
+            sizeUnit="px"
+            size={14}
+            color="#123abc"
+            loading={loading}
+          />
+          <BookList
+            books={books.filter(book => book.shelf === section.name)}
+            handleChange={handleChange}
+          />
+        </div>
+      </div>
+    ));
+
     return (
-      <div>
-        BookShelf
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {renderSections}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search" component={Search}>Add a book</Link>
+        </div>
       </div>
     );
   }
